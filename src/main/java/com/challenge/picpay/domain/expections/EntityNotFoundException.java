952b8913b5f5ceb9ff121ccf589cdb5033747340
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-01-02 19:25:01 -0300

    feat: updated expection with new constructor

--- src/main/java/com/challenge/picpay/domain/expections/EntityNotFoundException.java
+++ src/main/java/com/challenge/picpay/domain/expections/EntityNotFoundException.java
@@ -1,7 +1,12 @@
 package com.challenge.picpay.domain.expections;
 
+import java.util.UUID;
+
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
+    }
+    public EntityNotFoundException(String objectName, UUID id) {
+        super(objectName + " not found by ID: " + id);
     }
 }

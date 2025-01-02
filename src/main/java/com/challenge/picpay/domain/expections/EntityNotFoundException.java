bbecbcd4fc87e81ef81d70b5e94e90e6b5fd26d6
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-01-02 19:17:01 -0300

    feat: EntityNotFound custom exception

---
+++ src/main/java/com/challenge/picpay/domain/expections/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.challenge.picpay.domain.expections;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+}

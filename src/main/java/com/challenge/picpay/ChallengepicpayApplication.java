510e1218b07515b314d00ea417c70fb0ed57c2da
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-01-02 18:58:33 -0300

    first commit

---
+++ src/main/java/com/challenge/picpay/ChallengepicpayApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengepicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengepicpayApplication.class, args);
+	}
+
+}
